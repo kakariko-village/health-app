08f4c01ce2fcaa5fcf103f812af4784dee287180
Chau <levuminhchau90@gmail.com>
2023-07-19 20:50:19 +0700

    Config Tailwind and TS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,7 +13,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        dark: {
+          500: "#414141",
+          600: "#2E2E2E",
+        },
+        gray: {
+          400: "#777777",
+        },
+        primary: {
+          300: "#FFCC21",
+          400: "#FF963C",
+          500: "#EA6C00",
+        },
+        secondary: {
+          300: "#8FE9D0"
+        }
+      },
     },
   },
   plugins: [],
-}
+})
